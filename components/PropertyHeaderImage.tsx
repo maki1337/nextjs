86afaa0fc13df710794c8cb3a7c3c48a4ca63670
maki1337 <matevz.mak@gmail.com>
2024-10-13 21:32:50 +0200

    Property header

---
+++ components/PropertyHeaderImage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+interface InfoBoxProps {
+  image: React.ReactNode;
+}
+
+const PropertyHeaderImage: React.FC<InfoBoxProps> = ({ image }) => {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            src={`/images/${image}`}
+            alt=""
+            className="object-cover h-[400px] w-full"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertyHeaderImage;
