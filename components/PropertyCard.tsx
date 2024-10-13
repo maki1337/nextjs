16af59f616c0d5adf49c336a77341a99d65d5934
maki1337 <matevz.mak@gmail.com>
2024-10-13 15:45:35 +0200

    chamgomg project structure, adding property card

---
+++ components/PropertyCard.tsx
@@ -0,0 +1,116 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from "react-icons/fa";
+
+interface Location {
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
+interface Rates {
+  weekly: number;
+  monthly?: number;
+}
+
+interface SellerInfo {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+interface Property {
+  _id: string;
+  owner: string;
+  name: string;
+  type: string;
+  description: string;
+  location: Location;
+  beds: number;
+  baths: number;
+  square_feet: number;
+  amenities: string[];
+  rates: Rates;
+  seller_info: SellerInfo;
+  images: string[];
+  is_featured: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface PropertyCardProps {
+  property: Property;
+}
+
+const PropertyCard: React.FC<PropertyCardProps> = ({ property }) => {
+  return (
+    <div className="rounded-xl shadow-md relative">
+      <Image
+        src={`/images/${property.images[0]}`}
+        alt=""
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full h-auto rounded-t-xl"
+      />
+      <div className="p-4">
+        <div className="text-left md:text-center lg:text-left mb-6">
+          <div className="text-gray-600">Apartment</div>
+          <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+        </div>
+        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
+          $4,200/mo
+        </h3>
+
+        <div className="flex justify-center gap-4 text-gray-500 mb-4">
+          <p>
+            <i className="fa-solid fa-bed"></i> 3
+            <span className="md:hidden lg:inline">Beds</span>
+          </p>
+          <p>
+            <i className="fa-solid fa-bath"></i> 2
+            <span className="md:hidden lg:inline">Baths</span>
+          </p>
+          <p>
+            <i className="fa-solid fa-ruler-combined"></i>
+            1,500 <span className="md:hidden lg:inline">sqft</span>
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
+          <p>
+            <i className="fa-solid fa-money-bill"></i> Weekly
+          </p>
+          <p>
+            <i className="fa-solid fa-money-bill"></i> Monthly
+          </p>
+        </div>
+
+        <div className="border border-gray-100 mb-5"></div>
+
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
+            <i className="fa-solid fa-location-dot text-lg text-orange-700"></i>
+            <span className="text-orange-700"> Boston MA </span>
+          </div>
+          <a
+            href="property.html"
+            className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+          >
+            Details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyCard;
