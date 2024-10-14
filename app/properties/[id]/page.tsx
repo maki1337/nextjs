5b179923344455241a934c8dab85d1838b2f035a
maki1337 <matevz.mak@gmail.com>
2024-10-14 14:40:43 +0200

    property details

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,6 +3,7 @@ import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
 
 interface PropertyPageProps {
   params: {
@@ -14,6 +15,10 @@ const PropertyPage: React.FC<PropertyPageProps> = async ({ params }) => {
   await connectDB();
 
   const property = await Property.findById(params.id).lean();
+
+  if (!property) {
+    return <p>Property not found</p>;
+  }
   return (
     <>
       {property && <PropertyHeaderImage image={property.images[0]} />}
@@ -29,7 +34,9 @@ const PropertyPage: React.FC<PropertyPageProps> = async ({ params }) => {
       </section>
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
-          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+          </div>
         </div>
       </section>
     </>
