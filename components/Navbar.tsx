73b78cdf3c2a0a6b41ac7e07212230b0a38d969e
maki1337 <matevz.mak@gmail.com>
2024-10-15 11:22:34 +0200

    sign out

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -220,6 +220,10 @@ const Navbar = () => {
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex={-1}
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       id="user-menu-item-2"
                     >
                       Sign Out
